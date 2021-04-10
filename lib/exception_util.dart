9fe2c41269b864361bde2c4ef810256d9f289ec6
Ming Ming <nkming2@gmail.com>
2021-04-10 14:34:19 +0800

    Readable error for HTTP423

--- lib/exception_util.dart
+++ lib/exception_util.dart
@@ -11,6 +11,8 @@ String toUserString(dynamic exception, BuildContext context) {
   if (exception is ApiException) {
     if (exception.response.statusCode == 401) {
       return AppLocalizations.of(context).errorUnauthenticated;
+    } else if (exception.response.statusCode == 423) {
+      return AppLocalizations.of(context).errorLocked;
     }
   } else if (exception is SocketException) {
     return AppLocalizations.of(context).errorDisconnected;
