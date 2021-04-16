3e7caf744285e1666b6a70809064b250eaeb7bba
Ming Ming <nkming2@gmail.com>
2021-04-17 00:54:02 +0800

    Add Firebase Crashlytics

--- app/lib/object_extension.dart
+++ app/lib/object_extension.dart
@@ -3,4 +3,7 @@ import 'package:np_common/object_util.dart';
 extension ObjectExtension<T> on T {
   /// Deprecated, use [let]
   U run<U>(U Function(T obj) fn) => let(fn);
+
+  /// Return if this is contained inside [iterable]
+  bool isIn(Iterable<T> iterable) => iterable.contains(this);
 }
