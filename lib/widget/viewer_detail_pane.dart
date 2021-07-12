a0bbddfe3b608c6716cdc57bde560363290b2750
Ming Ming <nkming2@gmail.com>
2021-07-12 19:39:55 +0800

    Migrate deprecated dependency: android_intent

--- lib/widget/viewer_detail_pane.dart
+++ lib/widget/viewer_detail_pane.dart
@@ -1,4 +1,4 @@
-import 'package:android_intent/android_intent.dart';
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:exifdart/exifdart.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
