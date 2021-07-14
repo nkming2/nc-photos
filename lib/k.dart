c7734d836cde59bedb9277043df89bc9ff16c146
Ming Ming <nkming2@gmail.com>
2021-07-14 12:57:59 +0800

    Versioning: 21.3
    Identical to 21.2. Bumped version only because a misbuilt binary has been uploaded

--- lib/k.dart
+++ lib/k.dart
@@ -1,6 +1,6 @@
 /// Version string shown in settings page
-const versionStr = "21.2-f762eb";
-const version = 212;
+const versionStr = "21.3-f762eb";
+const version = 213;
 
 /// Show a snack bar for a short amount of time
 const snackBarDurationShort = const Duration(seconds: 4);
