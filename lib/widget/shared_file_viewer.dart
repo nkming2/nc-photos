5e100d1586c9e269957dcdb4139e391650c4d16c
Ming Ming <nkming2@gmail.com>
2021-10-09 16:34:27 +0800

    Ask to delete link share dir after unsharing

--- lib/widget/shared_file_viewer.dart
+++ lib/widget/shared_file_viewer.dart
@@ -10,12 +10,15 @@ import 'package:nc_photos/api/api_util.dart' as api_util;
 import 'package:nc_photos/app_localizations.dart';
 import 'package:nc_photos/cache_manager_util.dart';
 import 'package:nc_photos/entity/file.dart';
+import 'package:nc_photos/entity/file/data_source.dart';
 import 'package:nc_photos/entity/share.dart';
 import 'package:nc_photos/entity/share/data_source.dart';
 import 'package:nc_photos/exception_util.dart' as exception_util;
 import 'package:nc_photos/k.dart' as k;
+import 'package:nc_photos/remote_storage_util.dart' as remote_storage_util;
 import 'package:nc_photos/snack_bar_manager.dart';
 import 'package:nc_photos/theme.dart';
+import 'package:nc_photos/use_case/remove.dart';
 import 'package:nc_photos/use_case/remove_share.dart';
 import 'package:path/path.dart' as path;
 
@@ -210,7 +213,18 @@ class _SharedFileViewerState extends State<SharedFileViewer> {
       ));
       if (_shares.length == 1) {
         // removing last share
-        Navigator.of(context).pop();
+        try {
+          if (widget.file.path.startsWith(
+              remote_storage_util.getRemoteLinkSharesDir(widget.account) +
+                  "/")) {
+            // file is a link share dir created by us
+            if (await _askDeleteLinkShareDir()) {
+              await _deleteLinkShareDir();
+            }
+          }
+        } finally {
+          Navigator.of(context).pop();
+        }
       } else {
         setState(() {
           _shares.remove(share);
@@ -224,6 +238,49 @@ class _SharedFileViewerState extends State<SharedFileViewer> {
         duration: k.snackBarDurationNormal,
       ));
     }
+  }
+
+  Future<bool> _askDeleteLinkShareDir() async {
+    final result = await showDialog<bool>(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text(L10n.global().unshareLinkShareDirDialogTitle),
+        content: Text(L10n.global().unshareLinkShareDirDialogContent),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text(MaterialLocalizations.of(context).cancelButtonLabel)),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text(MaterialLocalizations.of(context).okButtonLabel)),
+        ],
+      ),
+    );
+    return result == true;
+  }
+
+  Future<void> _deleteLinkShareDir() {
+    // the upper dir is also part of this link share dir
+    var dirPath = path.dirname(widget.file.path);
+    // make sure we are not accidentally deleting other dirs
+    if (!dirPath.startsWith(
+        remote_storage_util.getRemoteLinkSharesDir(widget.account) + "/")) {
+      _log.shout(
+          "[_deleteLinkShareDir] Invalid upper dir to be deleted: $dirPath");
+      dirPath = widget.file.path;
+    }
+
+    final fileRepo = FileRepo(FileCachedDataSource());
+    return Remove(fileRepo, null)(
+      widget.account,
+      widget.file.copyWith(
+        path: dirPath,
+      ),
+    );
   }
 
   IconData? _getShareIcon(Share share) {
