d72f64d4f4eacabd988d559da0128c70f6337f9f
Ming Ming <nkming2@gmail.com>
2021-11-15 22:46:01 +0800

    Remove connectivity check on web
    It doesn't work at all on web

--- lib/connectivity_util.dart
+++ lib/connectivity_util.dart
@@ -1,7 +1,13 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/foundation.dart';
+import 'package:nc_photos/platform/k.dart' as platform_k;
 
 Future<void> waitUntilWifi({VoidCallback? onNoWifi}) async {
+  if (platform_k.isWeb) {
+    // connectivity does NOT work on web, currently it will always return mobile
+    // on Blink, and none on Gecko
+    return;
+  }
   while (true) {
     final result = await Connectivity().checkConnectivity();
     if (result == ConnectivityResult.wifi) {
