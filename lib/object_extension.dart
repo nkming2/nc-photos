67c35bcd00cec1d847684c258f6d17edd7cd7545
Ming Ming <nkming2@gmail.com>
2021-12-20 02:28:10 +0800

    Doc and tests for ObjectExtension

--- lib/object_extension.dart
+++ lib/object_extension.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
 
 extension ObjectExtension<T> on T {
+  /// Run [fn] with this, and return this
   T apply(void Function(T obj) fn) {
     fn(this);
     return this;
   }
 
+  /// Run [fn] with this, and return this
   Future<T> applyFuture(FutureOr<void> Function(T obj) fn) async {
     await fn(this);
     return this;
   }
 
+  /// Run [fn] with this, and return the results of [fn]
   U run<U>(U Function(T obj) fn) => fn(this);
 }
---
+++ test/object_extension_test.dart
@@ -0,0 +1,21 @@
+import 'package:nc_photos/object_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("ObjectExtension", () {
+    test("apply", () {
+      const obj = Object();
+      expect(obj.apply((obj) => 1), obj);
+    });
+
+    test("applyFuture", () async {
+      const obj = Object();
+      expect(await obj.applyFuture((obj) async => 1), obj);
+    });
+
+    test("run", () {
+      const obj = Object();
+      expect(obj.run((obj) => 1), 1);
+    });
+  });
+}
