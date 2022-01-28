2c62b353215b9ad8a7b1fba9e4dabbdf7ec3ce5c
Ming Ming <nkming2@gmail.com>
2022-01-29 00:39:14 +0800

    Fix username in bloc id being case sensitive

--- lib/bloc/bloc_util.dart
+++ lib/bloc/bloc_util.dart
@@ -1,7 +1,7 @@
 import 'package:nc_photos/account.dart';
 
 String getInstNameForAccount(String className, Account account) =>
-    "$className(${account.scheme}://${account.username}@${account.address})";
+    "$className(${account.scheme}://${account.username.toCaseInsensitiveString()}@${account.address})";
 
 String getInstNameForRootAwareAccount(String className, Account account) =>
-    "$className(${account.scheme}://${account.username}@${account.address}?${account.roots.join('&')})";
+    "$className(${account.scheme}://${account.username.toCaseInsensitiveString()}@${account.address}?${account.roots.join('&')})";
