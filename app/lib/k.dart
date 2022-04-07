b2dd68fc0cd274ec90c9c5e142376e98f3075928
Ming Ming <nkming2@gmail.com>
2022-04-08 03:11:34 +0800

    Versioning: 38.2

--- app/lib/k.dart
+++ app/lib/k.dart
@@ -1,6 +1,6 @@
 /// Version string shown in settings page
-const versionStr = "38.1";
-const version = 381;
+const versionStr = "38.2";
+const version = 382;
 
 /// Show a snack bar for a short amount of time
 const snackBarDurationShort = Duration(seconds: 4);
