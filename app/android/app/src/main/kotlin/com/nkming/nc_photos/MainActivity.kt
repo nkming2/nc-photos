8176e10dbe36316d13f2b01534923b6bc8ca10a3
Ming Ming <nkming2@gmail.com>
2022-04-19 14:01:49 +0800

    Format code

--- app/android/app/src/main/kotlin/com/nkming/nc_photos/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/nkming/nc_photos/MainActivity.kt
@@ -9,18 +9,30 @@ import io.flutter.plugin.common.MethodChannel
 class MainActivity : FlutterActivity() {
 	override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
 		super.configureFlutterEngine(flutterEngine)
-		MethodChannel(flutterEngine.dartExecutor.binaryMessenger,
-				MediaStoreChannelHandler.CHANNEL).setMethodCallHandler(
-				MediaStoreChannelHandler(this))
-		MethodChannel(flutterEngine.dartExecutor.binaryMessenger,
-				SelfSignedCertChannelHandler.CHANNEL).setMethodCallHandler(
-				SelfSignedCertChannelHandler(this))
-		MethodChannel(flutterEngine.dartExecutor.binaryMessenger,
-				ShareChannelHandler.CHANNEL).setMethodCallHandler(
-				ShareChannelHandler(this))
+		MethodChannel(
+			flutterEngine.dartExecutor.binaryMessenger,
+			MediaStoreChannelHandler.CHANNEL
+		).setMethodCallHandler(
+			MediaStoreChannelHandler(this)
+		)
+		MethodChannel(
+			flutterEngine.dartExecutor.binaryMessenger,
+			SelfSignedCertChannelHandler.CHANNEL
+		).setMethodCallHandler(
+			SelfSignedCertChannelHandler(this)
+		)
+		MethodChannel(
+			flutterEngine.dartExecutor.binaryMessenger,
+			ShareChannelHandler.CHANNEL
+		).setMethodCallHandler(
+			ShareChannelHandler(this)
+		)
 
-		EventChannel(flutterEngine.dartExecutor.binaryMessenger,
-				DownloadEventCancelChannelHandler.CHANNEL).setStreamHandler(
-				DownloadEventCancelChannelHandler(this))
+		EventChannel(
+			flutterEngine.dartExecutor.binaryMessenger,
+			DownloadEventCancelChannelHandler.CHANNEL
+		).setStreamHandler(
+			DownloadEventCancelChannelHandler(this)
+		)
 	}
 }
