0356e22be71cdeb7396d088e4e4de0ce9c2e20c9
Ming Ming <nkming2@gmail.com>
2022-04-23 01:53:38 +0800

    Fix permission error not handled correctly in some case

--- app/lib/mobile/file_saver.dart
+++ app/lib/mobile/file_saver.dart
@@ -1,7 +1,5 @@
 import 'dart:typed_data';
 
-import 'package:flutter/services.dart';
-import 'package:nc_photos/exception.dart';
 import 'package:nc_photos/mobile/android/media_store.dart';
 import 'package:nc_photos/platform/file_saver.dart' as itf;
 import 'package:nc_photos/platform/k.dart' as platform_k;
@@ -16,15 +14,6 @@ class FileSaver extends itf.FileSaver {
     }
   }
 
-  Future<String> _saveFileAndroid(String filename, Uint8List content) async {
-    try {
-      return await MediaStore.saveFileToDownload(filename, content);
-    } on PlatformException catch (e) {
-      if (e.code == MediaStore.exceptionCodePermissionError) {
-        throw PermissionException();
-      } else {
-        rethrow;
-      }
-    }
-  }
+  Future<String> _saveFileAndroid(String filename, Uint8List content) =>
+      MediaStore.saveFileToDownload(filename, content);
 }
