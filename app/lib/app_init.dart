0b81ab2a69f427cb0f41bc9484be7952e4c3141f
Ming Ming <nkming2@gmail.com>
2022-05-04 16:48:54 +0800

    Remove unnecessary import

--- app/lib/app_init.dart
+++ app/lib/app_init.dart
@@ -2,7 +2,6 @@ import 'package:equatable/equatable.dart';
 import 'package:event_bus/event_bus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kiwi/kiwi.dart';
 import 'package:logging/logging.dart';
