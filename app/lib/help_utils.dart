c6a21ee6065f8e68c2e899c05df0cbca3ea231a1
Ming Ming <nkming2@gmail.com>
2022-05-17 19:27:23 +0800

    Add donation link

--- app/lib/help_utils.dart
+++ app/lib/help_utils.dart
@@ -14,3 +14,5 @@ const editPhotosUrl = "https://bit.ly/3v82oKA";
 const collectionTypesUrl = "https://bit.ly/3OwSiNq";
 const contributorsUrl = "https://bit.ly/3QhlQQs";
 const videoPreviewUrl = "https://bit.ly/4c7cazP";
+
+const donateUrl = "https://bit.ly/3wQOHPZ";
