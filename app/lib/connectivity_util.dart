94e7c82709324985913c0b98ced7571a7f9a208e
Ming Ming <nkming2@gmail.com>
2022-06-07 19:55:54 +0800

    Remove obsolete code

--- app/lib/connectivity_util.dart
+++ app/lib/connectivity_util.dart
@@ -1,6 +1,4 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/foundation.dart';
-import 'package:nc_photos/async_util.dart' as async_util;
 import 'package:nc_photos/platform/k.dart' as platform_k;
 
 Future<bool> isWifi() async {
@@ -11,23 +9,4 @@ Future<bool> isWifi() async {
   }
   final result = await Connectivity().checkConnectivity();
   return result == ConnectivityResult.wifi;
-}
-
-Future<void> waitUntilWifi({VoidCallback? onNoWifi}) async {
-  if (platform_k.isWeb) {
-    // connectivity does NOT work on web, currently it will always return mobile
-    // on Blink, and none on Gecko
-    return;
-  }
-  await async_util.wait(
-    () async {
-      final result = await Connectivity().checkConnectivity();
-      if (result == ConnectivityResult.wifi) {
-        return true;
-      }
-      onNoWifi?.call();
-      return false;
-    },
-    pollInterval: const Duration(seconds: 5),
-  );
 }
