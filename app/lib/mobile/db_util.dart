093bc6e3275ce89b72c5fe7dd9d440fa669c3023
Ming Ming <nkming2@gmail.com>
2022-08-20 19:37:08 +0800

    Add reverse geocoder

--- app/lib/mobile/db_util.dart
+++ app/lib/mobile/db_util.dart
@@ -2,9 +2,11 @@ import 'dart:io' as dart;
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:path/path.dart' as path_lib;
 import 'package:path_provider/path_provider.dart';
-import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart' as sqlite3;
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart' as sql;
 
 Future<Map<String, dynamic>> getSqliteConnectionArgs() async {
   // put the database file, called db.sqlite here, into the documents folder
@@ -32,5 +34,27 @@ QueryExecutor openSqliteConnection() {
 }
 
 Future<void> applyWorkaroundToOpenSqlite3OnOldAndroidVersions() {
-  return sqlite3.applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+  return sql.applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+}
+
+Future<Database> openRawSqliteDbFromAsset(
+  String assetRelativePath,
+  String outputFilename, {
+  bool isReadOnly = false,
+}) async {
+  final dbFolder = await getApplicationDocumentsDirectory();
+  final file = dart.File(path_lib.join(dbFolder.path, outputFilename));
+  if (!await file.exists()) {
+    // copy file from assets
+    final blob = await rootBundle.load("assets/$assetRelativePath");
+    final buffer = blob.buffer;
+    await file.writeAsBytes(
+      buffer.asUint8List(blob.offsetInBytes, blob.lengthInBytes),
+      flush: true,
+    );
+  }
+  return sqlite3.open(
+    file.path,
+    mode: isReadOnly ? OpenMode.readOnly : OpenMode.readWriteCreate,
+  );
 }
