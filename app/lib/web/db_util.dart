e7f1354118f1b867c10cb8df62efc5575090512a
Ming Ming <nkming2@gmail.com>
2022-09-01 18:57:07 +0800

    Remove unused import

--- app/lib/web/db_util.dart
+++ app/lib/web/db_util.dart
@@ -2,7 +2,6 @@ import 'package:drift/drift.dart';
 import 'package:drift/wasm.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:http/http.dart' as http;
-import 'package:nc_photos/debug_util.dart';
 import 'package:nc_photos/entity/sqlite_table.dart' as sql;
 import 'package:sqlite3/wasm.dart';
 
