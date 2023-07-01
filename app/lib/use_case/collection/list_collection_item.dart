7d08d16af76da3e28933f3e32c39ee041b110523
Ming Ming <nkming2@gmail.com>
2023-07-01 22:38:38 +0800

    Remove obsolete code

--- app/lib/use_case/collection/list_collection_item.dart
+++ app/lib/use_case/collection/list_collection_item.dart
@@ -1,16 +1,11 @@
 import 'dart:async';
 
-import 'package:logging/logging.dart';
 import 'package:nc_photos/account.dart';
 import 'package:nc_photos/di_container.dart';
 import 'package:nc_photos/entity/collection.dart';
 import 'package:nc_photos/entity/collection/adapter.dart';
 import 'package:nc_photos/entity/collection_item.dart';
-import 'package:np_codegen/np_codegen.dart';
 
-part 'list_collection_item.g.dart';
-
-@npLog
 class ListCollectionItem {
   const ListCollectionItem(this._c);
 
