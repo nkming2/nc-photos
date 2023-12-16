f75b64812ac32fc3881623b205b6248692dc73a3
Ming Ming <nkming2@gmail.com>
2023-12-16 11:13:55 +0800

    Remove redundant import

--- app/lib/widget/sign_in.dart
+++ app/lib/widget/sign_in.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logging/logging.dart';
