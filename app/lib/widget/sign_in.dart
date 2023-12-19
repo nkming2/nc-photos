e1acc9799542bc019b041e2759b3329b7cf23057
Ming Ming <nkming2@gmail.com>
2023-12-19 21:09:49 +0800

    Revert "Remove redundant import"
    This reverts commit f75b64812ac32fc3881623b205b6248692dc73a3.

--- app/lib/widget/sign_in.dart
+++ app/lib/widget/sign_in.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logging/logging.dart';
