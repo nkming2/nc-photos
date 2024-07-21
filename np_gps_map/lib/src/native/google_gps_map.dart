14140c5ef082a8c0fd4de196e37cca66a70304fe
Ming Ming <nkming2@gmail.com>
2024-07-21 13:05:06 +0800

    Make google map in detail pane to support dark theme

--- np_gps_map/lib/src/native/google_gps_map.dart
+++ np_gps_map/lib/src/native/google_gps_map.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:np_gps_map/src/map_coord.dart';
 
@@ -38,6 +38,11 @@ class GoogleGpsMap extends StatelessWidget {
         ),
       },
       onTap: (_) => onTap?.call(),
+      onMapCreated: (controller) {
+        if (Theme.of(context).brightness == Brightness.dark) {
+          controller.setMapStyle(_mapStyleNight);
+        }
+      },
     );
   }
 
@@ -45,3 +50,7 @@ class GoogleGpsMap extends StatelessWidget {
   final double zoom;
   final VoidCallback? onTap;
 }
+
+// Generated in https://mapstyle.withgoogle.com/
+const _mapStyleNight =
+    '[{"elementType":"geometry","stylers":[{"color":"#242f3e"}]},{"elementType":"labels.text.fill","stylers":[{"color":"#746855"}]},{"elementType":"labels.text.stroke","stylers":[{"color":"#242f3e"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#d59563"}]},{"featureType":"poi","elementType":"labels.text.fill","stylers":[{"color":"#d59563"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#263c3f"}]},{"featureType":"poi.park","elementType":"labels.text.fill","stylers":[{"color":"#6b9a76"}]},{"featureType":"road","elementType":"geometry","stylers":[{"color":"#38414e"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#212a37"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#9ca5b3"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#746855"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#1f2835"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#f3d19c"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#2f3948"}]},{"featureType":"transit.station","elementType":"labels.text.fill","stylers":[{"color":"#d59563"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#17263c"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#515c6d"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#17263c"}]}]';
